479ea39fb1e60e94f6c9f764cc7480d3a05634a7
Ariful2634 <ariful2634@gmail.com>
2024-02-08 11:23:06 +0600

    update the incomplete status

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json())
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.stv3jdc.mongodb.net/?retryWrites=true&w=majority`;
 // console.log(uri)
 
@@ -43,6 +43,25 @@ async function run() {
         const result = await cursor.toArray()
         res.send(result)
       })
+
+
+    //   for change status
+      app.put('/task/status/:id',  async (req, res) => {
+        const id = req.params.id;
+        const filter = { _id: new ObjectId(id) }
+        const options = { upsert: true };
+        const updateStatus = req.body;
+        const status = {
+            $set: {
+
+                status: updateStatus.status,
+
+
+            }
+        }
+        const result = await qtaskCollection.updateOne(filter, status, options)
+        res.send(result)
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
